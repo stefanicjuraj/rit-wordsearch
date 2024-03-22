e8abfdd6ecc105c145bb0f3e26c6affbe8a7257e
stefanicjuraj <jurajstefanic@outlook.com>
2024-03-22 22:38:29 +0100

    feat(app): add Login, Navbar, Home, WaitingRoom, & GameRoom

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,5 +1,50 @@
+import { useEffect, useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// Auth
+import { User, onAuthStateChanged } from "firebase/auth";
+// Service
+import { auth } from "./services/firebase"
+// Components
+import Navbar from "./components/Navbar";
+
+import GameRoom from "./pages/GameRoom";
+// Pages
+import Login from "./pages/Login"
+import Home from "./pages/Home";
+import WaitingRoom from "./pages/WaitingRoom";
+
 export default function App() {
+  const [currentUser, setCurrentUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setCurrentUser(user);
+      } else {
+        setCurrentUser(null);
+      }
+    });
+
+    return unsubscribe;
+  }, []);
+
   return (
-    <></>
-  )
+    <Router>
+      <>
+        {currentUser ? (
+          <>
+            <Navbar />
+            <p> Welcome, {currentUser.email}</p>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/waiting-room" element={<WaitingRoom />} />
+              <Route path="/game-room" element={<GameRoom />} />
+            </Routes>
+          </>
+        ) : (
+          <Login />
+        )}
+      </>
+    </Router >
+  );
 }
