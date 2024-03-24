1b2c4cb3b55885d4ec0e0f947199bc9c0e95c4e1
stefanicjuraj <jurajstefanic@outlook.com>
2024-03-24 23:19:45 +0100

    feat(pages): add Link to Score page

--- app/src/pages/Home.tsx
+++ app/src/pages/Home.tsx
@@ -23,9 +23,17 @@ export default function Home() {
                     </p>
                 </div>
 
-                <Link to="/waiting-room" className="px-7 py-4 mt-8 mb-2 text-sm font-medium text-white bg-green-500 border border-gray-500 rounded-full focus:outline-none hover:bg-green-500 focus:ring-1 focus:ring-green-500 hover:text-black me-2">
-                    Enter Waiting Room
-                </Link>
+                <div className="mb-12">
+                    <Link to="/waiting-room" className="px-7 py-4 text-sm font-medium text-white bg-green-500 border border-gray-500 rounded-full focus:outline-none hover:bg-green-500 focus:ring-1 focus:ring-green-500 hover:text-black me-2">
+                        Enter Waiting Room
+                    </Link>
+                </div>
+
+                <div className="mt-12">
+                    <Link to="/score" className="px-7 py-4 text-sm font-medium text-white bg-yellow-500 border border-gray-500 rounded-full focus:outline-none hover:bg-yellow-500 focus:ring-1 focus:ring-yellow-500 hover:text-black me-2">
+                        View Scoreboard
+                    </Link>
+                </div>
 
                 <LogOut />
 
