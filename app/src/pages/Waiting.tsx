02d6b6e0670f72d4fe11a2666a77fa56127d9acc
stefanicjuraj <jurajstefanic@outlook.com>
2024-04-19 22:20:17 +0200

    feat(pages): add updatePlayerReadyStatus & handlePlayerReady button

--- app/src/pages/Waiting.tsx
+++ app/src/pages/Waiting.tsx
@@ -1,8 +1,8 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { DocumentData } from 'firebase/firestore';
 // Hooks
-import { getUsersinWaiting, joinWaiting, leaveWaiting } from '../hooks/useWaiting';
+import { getUsersinWaiting, joinWaiting, leaveWaiting, updatePlayerReadyStatus } from '../hooks/useWaiting';
 import { auth } from '../hooks/auth';
 // Components
 import Chat from '../components/Chat';
@@ -10,12 +10,9 @@ import Chat from '../components/Chat';
 export default function Waiting() {
     const [players, setPlayers] = useState<DocumentData[]>([]);
     const navigate = useNavigate();
-    const [countdown, setCountdown] = useState(30);
-
-    const intervalRef = useRef<NodeJS.Timeout | null>(null);
+    const currentUser = auth.currentUser;
 
     useEffect(() => {
-        const currentUser = auth.currentUser;
         if (currentUser) {
             joinWaiting(currentUser);
         }
@@ -23,31 +20,8 @@ export default function Waiting() {
         const unsubscribe = getUsersinWaiting((newUsers) => {
             setPlayers(newUsers);
 
-            if (newUsers.length === 2) {
-                if (intervalRef.current !== null) {
-                    clearInterval(intervalRef.current);
-                    intervalRef.current = null;
-                }
-                setCountdown(30);
-                intervalRef.current = setInterval(() => {
-                    setCountdown((currentCount) => {
-                        if (currentCount <= 1) {
-                            if (intervalRef.current) {
-                                clearInterval(intervalRef.current);
-                                intervalRef.current = null;
-                            }
-                            navigate('/game');
-                            return currentCount;
-                        }
-                        return currentCount - 1;
-                    });
-                }, 1000);
-            } else if (newUsers.length < 2) {
-                if (intervalRef.current) {
-                    clearInterval(intervalRef.current);
-                    intervalRef.current = null;
-                }
-                setCountdown(30);
+            if ((newUsers as DocumentData[]).length === 2 && (newUsers as DocumentData[]).every(user => user.ready)) {
+                navigate('/game');
             }
         });
 
@@ -56,34 +30,44 @@ export default function Waiting() {
                 leaveWaiting(currentUser.uid);
             }
             unsubscribe();
-            if (intervalRef.current) {
-                clearInterval(intervalRef.current);
-                intervalRef.current = null;
-            }
         };
-    }, [navigate]);
+    }, [navigate, currentUser]);
+
+    const handlePlayerReady = () => {
+        if (currentUser) {
+            const userId = currentUser.uid || '';
+            updatePlayerReadyStatus(userId);
+        }
+    };
 
     return (
         <div className="mx-auto mt-32 text-white">
             <h1 className="p-2 mb-2 text-xl text-center">
                 Players in the waiting room: {players.length}
             </h1>
-            {players.length === 2 &&
-                <p className="text-center">
-                    Game starts in: {countdown} seconds
-                </p>
-            }
             <ul className="text-center text-sm">
-                {players.map((players, index) => (
+                {players.map((player, index) => (
                     <li key={index}>
-                        <span className="text-green-500">•</span> {players.email}
-                        {/* {user.joinedAt && new Date(user.joinedAt.seconds * 1000).toDateString()} */}
+                        <span className="text-green-500">•</span> {player.email} - {player.ready ? "Ready" : "Not Ready"}
                     </li>
                 ))}
             </ul>
+            <div className="text-center my-8">
+                <button
+                    className="px-7 py-4 text-sm font-medium text-white bg-green-500 border border-green-500 rounded-full focus:outline-none hover:bg-green-500 focus:ring-1 focus:ring-green-500 hover:text-black"
+                    onClick={handlePlayerReady}
+                >
+                    Ready
+                </button>
+            </div>
+            {players.length === 2 && (
+                <p className="text-center mb-4">
+                    Waiting for both players to be ready...
+                </p>
+            )}
             <Chat />
             <div className="text-center mb-32">
-                <Link to="/" className="px-7 py-4 text-sm font-medium text-white bg-red-500 border border-red-500 rounded-full focus:outline-none hover:bg-red-500 focus:ring-1 focus:ring-red-500 hover:text-black me-2">
+                <Link to="/" className="px-7 py-4 text-sm font-medium text-white bg-red-500 border border-red-500 rounded-full focus:outline-none hover:bg-red-500 focus:ring-1 focus:ring-red-500 hover:text-black">
                     Exit Waiting Room
                 </Link>
             </div>
