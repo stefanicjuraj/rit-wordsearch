c9405e8ba80186f8afcdf788ed7b25537c071c1b
stefanicjuraj <jurajstefanic@outlook.com>
2024-04-24 22:10:07 +0200

    feat(components): add GameOver

---
+++ app/src/components/GameOver.tsx
@@ -0,0 +1,9 @@
+export default function GameOver() {
+
+    return (
+        <div className="mx-auto mt-24 mb-16 text-center text-white max-w-screen-md">
+            <h1 className="mb-4 text-2xl font-bold text-center">Game Over</h1>
+        </div>
+    );
+
+}
