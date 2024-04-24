a55027b3b796f9cc254b6af6d892f0eae65a2ed8
stefanicjuraj <jurajstefanic@outlook.com>
2024-04-24 22:11:17 +0200

    refactor(pages): replace Score content with ScoreBoard component

--- app/src/pages/Score.tsx
+++ app/src/pages/Score.tsx
@@ -1,79 +1,9 @@
-import { useEffect, useState } from 'react';
-import { doc, getDoc } from 'firebase/firestore';
-// Services
-import { db } from "../services/firebase";
-// Types
-import { UserScore } from '../types/score';
-import { Link } from 'react-router-dom';
-// Components
-import Latest from '../components/LatestScore';
+import ScoreBoard from "../components/ScoreBoard";
 
 export default function Score() {
-    const [scores, setScores] = useState<UserScore[]>([]);
-
-    useEffect(() => {
-        const fetchScores = async () => {
-            const scoreRef = doc(db, "collection", "scores");
-
-            const docSnap = await getDoc(scoreRef);
-            if (docSnap.exists()) {
-                const scoresData = docSnap.data();
-
-                const scoresArray: UserScore[] = Object.entries(scoresData).map(([, userScores]: [string, unknown]) => {
-                    return {
-                        displayName: (userScores as UserScore).displayName || 'Unknown',
-                        highestScore: (userScores as UserScore).highestScore || 0
-                    };
-                });
-
-                scoresArray.sort((a, b) => b.highestScore - a.highestScore);
-                setScores(scoresArray);
-            } else {
-                console.log("No scores found!");
-            }
-        };
-
-        fetchScores().catch(console.error);
-    }, []);
-
     return (
-
         <>
-            <div className="mx-auto mt-24 mb-16 text-center text-white max-w-screen-md">
-                <h1 className="mb-4 text-2xl font-bold text-center">Highest Game Scores</h1>
-            </div>
-            <div className="relative overflow-x-auto max-w-screen-sm mx-auto rounded-xl">
-                <table className="w-full text-sm text-left rtl:text-right text-gray-500">
-                    <thead className="text-sm text-white uppercase bg-green-500">
-                        <tr>
-                            <th scope="col" className="px-6 py-7">
-                                Player
-                            </th>
-                            <th scope="col" className="px-6 py-7">
-                                Highest Score
-                            </th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {scores.map((user, index) => (
-                            <tr key={index} className="bg-white border-b">
-                                <th scope="row" className="px-6 py-5 font-medium text-black whitespace-nowrap">
-                                    {user.displayName}
-                                </th>
-                                <td className="px-6 py-4 text-black">
-                                    {user.highestScore}
-                                </td>
-                            </tr>
-                        ))}
-                    </tbody>
-                </table>
-            </div>
-            <Latest />
-            <div className="mt-24 text-center mb-32">
-                <Link to="/" className="px-7 py-4 text-sm font-medium text-white bg-red-500 border border-red-500 rounded-full focus:outline-none hover:bg-red-500 focus:ring-1 focus:ring-red-500 hover:text-black me-2">
-                    Exit
-                </Link>
-            </div>
+            <ScoreBoard users={[]} />
         </>
-    );
+    )
 }
